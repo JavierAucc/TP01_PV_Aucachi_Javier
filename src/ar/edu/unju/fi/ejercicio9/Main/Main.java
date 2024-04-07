2949441532183a42ded0ac1f4e66f295e20c9c87
Javi <Javi@DESKTOP-JM6NVPK>
2024-04-07 12:01:36 -0300

    ejercicio 9 terminado

---
+++ src/ar/edu/unju/fi/ejercicio9/Main/Main.java
@@ -0,0 +1,41 @@
+package ar.edu.unju.fi.ejercicio9.Main;
+
+import java.util.Scanner;
+
+import ar.edu.unju.fi.ejercicio9.Model.Producto;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		Scanner scanner = new Scanner(System.in);
+		int n = 3 ;
+		while(n>0) {
+			
+			Producto producto = new Producto();
+			
+			System.out.println("\n Ingrese Nombre del Producto: ");
+			String nombre = scanner.next();
+			System.out.println("\n Ingrese Codigo del Producto: ");
+			String codigo = scanner.next();
+			System.out.println("\n Ingrese Precio del Producto: ");
+			Float precio = scanner.nextFloat();
+			System.out.println("\n Ingrese Descuento del Producto: ");
+			float descuento = scanner.nextFloat();
+			
+			producto.setNombre(nombre);
+			producto.setCodigo(codigo);
+			producto.setPrecio(precio);
+			producto.setDescuento(descuento);
+			
+			producto.MostrarDatos();
+			
+			n--;
+			
+		}
+		
+		
+	}
+
+}
---
+++ src/ar/edu/unju/fi/ejercicio9/Model/Producto.java
@@ -0,0 +1,70 @@
+package ar.edu.unju.fi.ejercicio9.Model;
+
+public class Producto {
+	private String nombre;
+	private String codigo;
+	private float precio;
+	private float descuento;
+	
+	public Producto() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public float getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+
+	public float getDescuento() {
+		return descuento;
+	}
+
+	public void setDescuento(float descuento) {
+		this.descuento = descuento;
+	}
+	
+	public float calcularDescuento() {
+		float desc = this.descuento;
+		float total = this.precio;
+		if(this.descuento >=0 && this.descuento <= 50) {
+			float aux = (this.precio*desc)/100;
+			total -= aux;
+		}
+		return total;
+	}
+	
+	public void MostrarDatos() {
+		System.out.println("\n Producto: "+this.nombre);
+		System.out.println("\n Codigo: "+this.codigo);
+		System.out.println("\n Precio: "+this.precio);
+		if(this.descuento<1 || this.descuento>50) {
+		System.out.println("\n Descuento: Sin Descuento");
+		}
+		else {
+			System.out.println("\n Descuento: %"+this.descuento);
+			System.out.println("\n Precio con Descuento: "+calcularDescuento());
+			
+		}
+		
+	}
+	
+}
