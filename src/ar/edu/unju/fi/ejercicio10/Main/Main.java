5e7806cd95297a48ab1f5e2843e83a239633abae
Javi <Javi@DESKTOP-JM6NVPK>
2024-04-09 12:40:02 -0300

    ejercicio 10 terminado

---
+++ src/ar/edu/unju/fi/ejercicio10/Main/Main.java
@@ -0,0 +1,41 @@
+package ar.edu.unju.fi.ejercicio10.Main;
+
+import java.util.Scanner;
+
+import ar.edu.unju.fi.ejercicio10.Model.Pizza;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 1;
+		Scanner scanner = new Scanner(System.in);
+		
+		while(n != 4) {
+			
+			System.out.println("\n Ingrese Diametro de Pizza");
+			int diametro = scanner.nextInt();
+			System.out.println("Ingredientes Especiales? True/False");
+			boolean ingredienteEspecial = scanner.nextBoolean();
+			
+			if(diametro == 20 || diametro == 30 || diametro == 40) {
+				
+				Pizza pizza = new Pizza();
+				pizza.setDiametro(diametro);
+				pizza.setIngredienteEspecial(ingredienteEspecial);
+				pizza.calcularPrecio();
+				System.out.println(" *** Pizza N° "+n);
+				pizza.mostrarDatos();
+				n++;
+			}
+			else {
+				System.out.println("\n Diametro de Pizza Incorrecto:");
+				System.out.println("Diametro validos : 20, 30, 40 !");
+			}
+			
+		}
+		
+		
+	}
+
+}
---
+++ src/ar/edu/unju/fi/ejercicio10/Model/Pizza.java
@@ -0,0 +1,70 @@
+package ar.edu.unju.fi.ejercicio10.Model;
+
+public class Pizza {
+	private int diametro;
+	private float precio;
+	private double area;
+	private boolean ingredienteEspecial;
+	
+	final int ADICIONAL20 = 500;
+	final int ADICIONAL30 = 750;
+	final int ADICIONAL40 = 1000;
+	
+	public Pizza() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public float getDiametro() {
+		return diametro;
+	}
+	public void setDiametro(int diametro) {
+		this.diametro = diametro;
+	}
+	public boolean isIngredienteEspecial() {
+		return ingredienteEspecial;
+	}
+	public void setIngredienteEspecial(boolean ingredienteEspecial) {
+		this.ingredienteEspecial = ingredienteEspecial;
+	}
+	
+	public float calcularPrecio() {
+		float precio = 0;
+		switch(this.diametro) {
+			case 20 :
+				precio = 4500;
+					if(this.ingredienteEspecial) {
+						precio += ADICIONAL20;
+					}
+			break;
+			case 30 :
+				 precio = 4800;
+					if(this.ingredienteEspecial) {
+						precio += ADICIONAL30;
+					}
+			break;
+			case 40 :
+				 precio = 5500;
+					if(this.ingredienteEspecial) {
+						precio += ADICIONAL30;
+					}
+			break;
+				
+		}
+		return precio;
+	}
+	
+	public double calcularArea() {
+		float radio = this.diametro / 2 ;
+		this.area = Math.PI*(Math.pow(radio,2));
+		return this.area;
+	}
+	
+	public void mostrarDatos() {
+		System.out.println("\n Diametro : "+this.diametro);
+		System.out.println("\n Ingredientes Especiales : "+this.ingredienteEspecial);
+		System.out.println("\n Precio Pizza : "+calcularPrecio());
+		calcularArea();
+		System.out.println("\n Area de la Pizza : "+this.area);
+	}
+	
+}
