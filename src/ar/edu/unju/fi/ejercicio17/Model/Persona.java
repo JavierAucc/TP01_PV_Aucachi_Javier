4a1390aa06b81e9a5aa2d3879462b95ec6ab5928
Javi <Javi@DESKTOP-JM6NVPK>
2024-04-13 19:46:35 -0300

    ejercicios 12-13-14-15-16 terminados, aun falta refinarlos

---
+++ src/ar/edu/unju/fi/ejercicio17/Model/Persona.java
@@ -0,0 +1,5 @@
+package ar.edu.unju.fi.ejercicio17.Model;
+
+public class Persona {
+
+}
---
+++ src/ar/edu/unju/fi/ejercicio18/Main/Main.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.fi.ejercicio18.Main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
